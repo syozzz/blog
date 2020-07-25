790c5b0ac6b709c2d966139165a5544eac4ff7ac
syozzz <15228134593@163.com>
2020-07-25 18:12:50 +0800

    fixed bug
    修复搜索组件 url  固定死的问题，兼容多域名部署

--- themes/Sakura/source/js/InsightSearch.js
+++ themes/Sakura/source/js/InsightSearch.js
@@ -39,7 +39,7 @@
             case 'CATEGORIES':
             case 'TAGS':
                 $searchItems = array.map(function (item) {
-                    return searchItem(type === 'CATEGORIES' ? 'folder' : 'tag', item.name, item.slug, null, item.permalink);
+                    return searchItem(type === 'CATEGORIES' ? 'folder' : 'tag', item.name, item.slug, null, item.permalink.replace(location.origin, ''));
                 });
                 break;
             default:
